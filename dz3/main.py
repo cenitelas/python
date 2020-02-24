eede0d16b47bcbb9c585a8df4c9efa7901587320
АтчановА <cenitelas@mail.ru>
2020-02-24 19:50:30 +0600

    +

--- dz3/main.py
+++ dz3/main.py
@@ -7,10 +7,14 @@ def check():
             result = float(params[0])*currency[params[1].lower()][params[2].lower()]
             print(result)
         if len(params) == 4:
-            if currency.get(params[1]):
-                currency[params[3]]=float(params[2])
+            if currency.get(params[1].lower()):
+                currency[params[3].lower()]=float(params[2])
             else:
-                currency[params[1]]={params[3]:float(params[2])}
+                currency[params[1].lower()]={params[3].lower():float(params[2])}
+            if currency.get(params[3].lower()):
+                currency[params[1].lower()]=1/float(params[2])
+            else:
+                currency[params[3].lower()]={params[1].lower():1/float(params[2])}
             print("OK")
     except:
         print("Нет такой валютной пары")
